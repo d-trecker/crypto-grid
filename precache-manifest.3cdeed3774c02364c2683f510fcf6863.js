1c025c267aafbbb9b1a92dcd63c524bec1254616
Dylan <79778111+d-trecker@users.noreply.github.com>
2021-10-19 16:23:34 -0700

    Updates

---
+++ precache-manifest.3cdeed3774c02364c2683f510fcf6863.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c3c4e888c884c4648a0c8c4a6d6dbe",
+    "url": "/crypto-grid/index.html"
+  },
+  {
+    "revision": "f36398d7ef039c5d2dbb",
+    "url": "/crypto-grid/static/css/2.f7861fd5.chunk.css"
+  },
+  {
+    "revision": "be4b4e5ce44ea1a2b658",
+    "url": "/crypto-grid/static/css/main.84d5ed5e.chunk.css"
+  },
+  {
+    "revision": "f36398d7ef039c5d2dbb",
+    "url": "/crypto-grid/static/js/2.9cb0cfed.chunk.js"
+  },
+  {
+    "revision": "f079ba4f3fe2a4cf26004adb08761d61",
+    "url": "/crypto-grid/static/js/2.9cb0cfed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4b4e5ce44ea1a2b658",
+    "url": "/crypto-grid/static/js/main.b47ec06f.chunk.js"
+  },
+  {
+    "revision": "4ad8025356cfd2e22f79",
+    "url": "/crypto-grid/static/js/runtime-main.43e4c523.js"
+  },
+  {
+    "revision": "8b4f4718b02a834f95b7eabe2b82d9a5",
+    "url": "/crypto-grid/static/media/Tr2n.8b4f4718.ttf"
+  }
+]);
